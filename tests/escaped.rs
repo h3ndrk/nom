d9126ae2256e69902141a2af18dc0ae175e03ae8
Hendrik <git@h3ndrk.de>
2022-09-08 23:49:07 +0200

    Add hanging test case

--- tests/escaped.rs
+++ tests/escaped.rs
@@ -1,7 +1,12 @@
-use nom::bytes::complete::escaped;
-use nom::character::complete::digit1;
-use nom::character::complete::one_of;
-use nom::{error::ErrorKind, Err, IResult};
+use nom::{
+  branch::alt,
+  bytes::complete::{escaped, escaped_transform, tag, take_while},
+  character::complete::{digit1, one_of},
+  combinator::value,
+  error::ErrorKind,
+  sequence::delimited,
+  Err, IResult,
+};
 
 fn esc(s: &str) -> IResult<&str, &str, (&str, ErrorKind)> {
   escaped(digit1, '\\', one_of("\"n\\"))(s)
@@ -9,7 +14,6 @@ fn esc(s: &str) -> IResult<&str, &str, (&str, ErrorKind)> {
 
 #[cfg(feature = "alloc")]
 fn esc_trans(s: &str) -> IResult<&str, String, (&str, ErrorKind)> {
-  use nom::bytes::complete::{escaped_transform, tag};
   escaped_transform(digit1, '\\', tag("n"))(s)
 }
 
@@ -21,8 +25,22 @@ fn test_escaped() {
 #[test]
 #[cfg(feature = "alloc")]
 fn test_escaped_transform() {
+  use nom::error::Error;
+
   assert_eq!(
     esc_trans("abcd"),
     Err(Err::Error(("abcd", ErrorKind::EscapedTransform)))
+  );
+  assert_eq!(
+    delimited::<_, _, _, _, Error<&str>, _, _, _>(
+      tag("\""),
+      escaped_transform(
+        take_while(|character: char| character.is_alphanumeric()),
+        '\\',
+        alt((value("\\", tag("\\")), value("\"", tag("\""))))
+      ),
+      tag("\"")
+    )("\"foo\""),
+    Ok(("", "foo".to_string()))
   );
 }
